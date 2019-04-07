51a14fd2cc73a3711e92f5b44d2d55848828bf71
gallorob <32876132+gallorob@users.noreply.github.com>
2019-04-07 18:05:19 +0200

    sudo checks

--- tbw.sh
+++ tbw.sh
@@ -1,1 +1,7 @@
+# Don't waste resources if user is not sudoer
+if [ "$EUID" -ne 0 ]
+    then echo "Run as sudo."
+    exit
+fi
+
 echo "GB Written: $(echo "scale=3; $(sudo /usr/sbin/smartctl -A /dev/sda | grep "Total_LBAs_Written" | awk '{print $10}') * 512 / 1073741824" | bc | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta')"
